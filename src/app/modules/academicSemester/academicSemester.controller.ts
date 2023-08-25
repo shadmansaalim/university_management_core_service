c50e18766b680411e932facabc87b010262914e1
shadmansaalim <shadmansaalim999@gmail.com>
2023-08-25 14:43:36 +0600

    Unecessary try catch block removed from controller function

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,33 +1,27 @@
 // Imports
 import { AcademicSemester } from '@prisma/client';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './academicSemester.service';
 
 // Create Semester Controller Function
-const insertIntoDB = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // Destructuring Academic Semester data from request body
-      const { ...academicSemesterData } = req.body;
-      const result = await AcademicSemesterService.insertIntoDB(
-        academicSemesterData
-      );
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  // Destructuring Academic Semester data from request body
+  const { ...academicSemesterData } = req.body;
+  const result = await AcademicSemesterService.insertIntoDB(
+    academicSemesterData
+  );
 
-      // Sending API Response
-      sendResponse<AcademicSemester>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: 'Academic Semester created successfully.',
-        data: result,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+  // Sending API Response
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester created successfully.',
+    data: result,
+  });
+});
 
 export const AcademicSemesterController = {
   insertIntoDB,
