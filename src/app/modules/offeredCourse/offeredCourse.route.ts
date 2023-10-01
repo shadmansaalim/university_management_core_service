b1981b0df5a46e3abecb3847db8ed7af970f04bb
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-01 18:03:34 +1100

    Offered Course Section model created

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -1,5 +1,7 @@
 // Imports
 import express from 'express';
+import { ENUM_USER_ROLES } from '../../../enums/user';
+import authGuard from '../../middlewares/authGuard';
 import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseController } from './offeredCourse.controller';
 import { OfferedCourseValidation } from './offeredCourse.validation';
@@ -20,14 +22,14 @@ router.post(
 
 router.patch(
   '/:id',
-  // authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
+  authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
   validateRequest(OfferedCourseValidation.updateOfferedCourseZodSchema),
   OfferedCourseController.updateSingleOfferedCourse
 );
 
 router.delete(
   '/:id',
-  // authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
+  authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
   OfferedCourseController.deleteSingleOfferedCourse
 );
 
